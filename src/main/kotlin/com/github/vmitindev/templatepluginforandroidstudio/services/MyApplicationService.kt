47356f401a0bb8ad56d14ea241078322b11a921c
GitHub Action <action@github.com>
2022-06-03 08:32:10 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/vmitindev/templatepluginforandroidstudio/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vmitindev.templatepluginforandroidstudio.services
+
+import com.github.vmitindev.templatepluginforandroidstudio.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/vmitindev/templatepluginforandroidstudio/services/MyProjectService.kt
+++ src/main/kotlin/com/github/vmitindev/templatepluginforandroidstudio/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.vmitindev.templatepluginforandroidstudio.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vmitindev.templatepluginforandroidstudio.MyBundle
 
 class MyProjectService(project: Project) {
 
